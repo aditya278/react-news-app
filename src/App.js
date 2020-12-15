0f3548f360b02770dac0633739419565910704ba
aditya278 <aditya.shukla278@gmail.com>
2020-12-15 13:53:50 +0530

    Modified he home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-import Header from './components/Header';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 //import Carousel from './components/Carousel';
-import HeadlinePosts from './components/HeadlinePosts';
+import HeadlinePosts from "./components/HeadlinePosts";
 
 function App() {
-
-  const [countryCd, setCountryCd] = useState('');
+  const [countryCd, setCountryCd] = useState("");
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -21,33 +22,38 @@ function App() {
         const res = await axios.get("http://ip-api.com/json");
         setCountryCd(res.data.countryCode);
 
-        if(countryCd !== '') {
-          const url = 'http://newsapi.org/v2/top-headlines?' +
-          `country=${countryCd}&` +
-          'apiKey=a48cfcb6549847c285555f8d280213b9';
+        if (countryCd !== "") {
+          const url =
+            "http://newsapi.org/v2/top-headlines?" +
+            `country=${countryCd}&` +
+            "apiKey=a48cfcb6549847c285555f8d280213b9";
           const res2 = await axios.get(url);
 
           setArticles(res2.data.articles);
           setLoading(false);
         }
-      
-      }
-      catch(err) {
+      } catch (err) {
         console.log(err.message);
       }
     }
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [countryCd]);
-  
+
   return (
-    <div className="container">
-      <Header />
-      <Navbar />
-      <hr />
-      <HeadlinePosts articles={articles.slice(1,4)} loading={loading}/>
-      <hr />
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+        <Navbar />
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <HeadlinePosts articles={articles.slice(0, 3)} loading={loading} />
+          </Route>
+        </Switch>
+        <hr />
+      </div>
+    </Router>
   );
 }
 
--- src/components/HeadlinePosts.js
+++ src/components/HeadlinePosts.js
@@ -12,30 +12,30 @@ export default function HeadlinePosts({articles, loading}) {
     if(loading) {
         return <Loading />
     }
-
+    
     return (
         <div className="row">
-            <div className="col-8 card mb-3 w-75">
-                <img className="card-img-top w-100 pt-2" src={articles[0].urlToImage} alt={articles[0].source.name} />
+            <div className="col-lg-8 col-sm-12 card mb-3">
+                <img className="card-img-top w-100 pt-2" src={articles[0].urlToImage || `https://source.unsplash.com/100x50/?${articles[0].source.name},news`} alt={articles[0].source.name} />
                 <div className="card-body">
-                    <h5 className="card-title">{articles[0].title}</h5>
+                    <a href={articles[0].url}><h5 className="card-title">{articles[0].title}</h5></a>
                     <br />
                     <p className="card-text">{articles[0].description}</p>
                     <p className="card-text"><small className="text-muted">{convertDate(articles[0].publishedAt)} <b>Source:</b> {articles[0].source.name}</small></p>
                 </div>
             </div>
-            <div className="col-4 w-25">
-                <div className="row-6 card mb-3 pt-2">
-                    <img className="card-img-top w-100" src={articles[1].urlToImage} alt={articles[1].source.name} />
+            <div className="col-lg-4 col-sm-12">
+                <div className="col-12 card mb-3 pt-2">
+                    <img className="card-img-top w-100 pt-2" src={articles[1].urlToImage || `https://source.unsplash.com/100x50/?${articles[1].source.name},news`} alt={articles[1].source.name} />
                     <div className="card-body">
-                        <h5 className="card-title">{articles[1].title}</h5>
+                        <a href={articles[1].url}><h5 className="card-title">{articles[1].title}</h5></a>
                         <p className="card-text"><small className="text-muted">{convertDate(articles[1].publishedAt)} <b>Source:</b> {articles[1].source.name}</small></p>
                     </div>
                 </div>
-                <div className="row-6 card mb-3 pt-2">
-                    <img className="card-img-top w-100" src={articles[2].urlToImage || `https://source.unsplash.com/100x50/?${articles[2].source.name}`} alt={articles[2].source.name} script={{width : '100%'}} />
+                <div className="col-12 card mb-3 pt-2">
+                    <img className="card-img-top w-100" src={articles[2].urlToImage || `https://source.unsplash.com/100x50/?${articles[2].source.name},news`} alt={articles[2].source.name} script={{width : '100%'}} />
                     <div className="card-body">
-                        <h5 className="card-title">{articles[2].title}</h5>
+                        <a href={articles[2].url}><h5 className="card-title">{articles[2].title}</h5></a>
                         <p className="card-text"><small className="text-muted">{convertDate(articles[2].publishedAt)} <b>Source:</b>{articles[2].source.name}</small></p>
                     </div>
                 </div>
